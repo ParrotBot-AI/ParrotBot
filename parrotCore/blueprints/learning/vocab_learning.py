6345db1dc76d05b3b9c4db4f0d1b672e7a2e3274
hectorrrrr <zhilinhe@usc.edu>
2024-03-19 00:05:57 -0700

    c

--- parrotCore/blueprints/learning/vocab_learning.py
+++ parrotCore/blueprints/learning/vocab_learning.py
@@ -218,7 +218,7 @@ def words_gpt_fetch(
                     "output": "clientId"
                 },
                 "streaming":{
-                    "url": "http://localhost:8000/v1/modelapi/getVocabContent/{ClientID}/",
+                    "url": "http://54.169.8.123:57875/v1/modelapi/getVocabContent/{ClientID}/",
                     "method": "sse",
                 }
             },
